1a544fcd917f4cc28141e27f3114581715f54c48
gibranJLAS <gibranadnan1010@gmail.com>
2026-07-19 12:08:46 +0800

    updateing NoclipHack.lua

--- Funcs/NoclipHack.lua
+++ Funcs/NoclipHack.lua
@@ -3,7 +3,7 @@ local RunService = game:GetService("RunService")
 
 local player = Players.LocalPlayer
 local character = player.Character or player.CharacterAdded:Wait()
-local noclip = false
+noclip = false
 
 -- 2. LOGIKA RESPOND CHARACTER
 player.CharacterAdded:Connect(function(newCharacter)
@@ -12,11 +12,11 @@ end)
 
 -- 4. PROSES NOCLIP
 RunService.Stepped:Connect(function()
-	if noclip and character then
-		for _, part in pairs(character:GetDescendants()) do
-			if part:IsA("BasePart") and part.CanCollide then
-				part.CanCollide = false
-			end
-		end
-	end
+    if character then
+        for _, part in pairs(character:GetDescendants()) do
+            if part:IsA("BasePart") then
+                part.CanCollide = not noclip
+            end
+        end
+    end
 end)
