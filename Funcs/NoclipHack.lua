46727236c86523a178446bfe1b2584d7232be303
gibranJLAS <gibranadnan1010@gmail.com>
2026-07-20 19:00:35 +0800

    Add files via upload

--- Funcs/NoclipHack.lua
+++ Funcs/NoclipHack.lua
@@ -1,15 +1,26 @@
 local player = game:GetService("Players").LocalPlayer
 local runservice = game:GetService("RunService")
 
+local originalCollision = {}
 local isnoclipping = false
 
 runservice.Stepped:Connect(function()
-    if player.Character then
-        if isnoclipping == true then
-            for i, v in pairs(player.Character:GetDescendants()) do
-               if v:IsA("BasePart") then
-                   v.CanCollide = false
-                 end
+    if not player.Character then
+        return
+    end
+
+    for _, v in ipairs(player.Character:GetDescendants()) do
+        if v:IsA("BasePart") then
+            if isnoclipping then
+                if originalCollision[v] == nil then
+                    originalCollision[v] = v.CanCollide
+                end
+
+                v.CanCollide = false
+            else
+                if originalCollision[v] ~= nil then
+                    v.CanCollide = originalCollision[v]
+                end
             end
         end
     end
